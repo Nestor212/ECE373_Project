18b890ced8e8b919a6d175a2b25c4f51ef7090f7
Tara <70345904+tbrady123@users.noreply.github.com>
2023-12-06 15:13:40 -0700

    Update SupplyManagerClient.java
    fixed very minor typo

--- src/gui/SupplyManagerClient.java
+++ src/gui/SupplyManagerClient.java
@@ -4,16 +4,16 @@ package gui;
  * Class Description
  * Part of the GUI package (detailed below), this instantiates the GUI object.
  *
- * System can be stared by running this file IF 'driver.java' has already been run once, 
- * since the driver instantiates then saves a company object with all its constituents. 
+ * System can be started by running this file IF 'driver.java' has already been run once, 
+ * since the driver instantiates then saves a company object with all of its constituents. 
  * 
  * GUI Package:
- * The GUI package is made up of various files that are extensions of the Java swing frame class. 
- * Each file is named after it's corresponding function + Frame and instantiates a new window. 
+ * The GUI package is comprised of various files that are extensions of the Java swing frame class. 
+ * Each file is named after its corresponding function + Frame and instantiates a new window. 
  * 
  * The system begins by presenting the LoginFrame. 
  * Upon a successful login, the loginFrame is discarded and a HomeFrame Window appears, tailored to the account type that has logged in. 
- * From there, the various options that appear will instantiate a window/Frame corresponding to a file in the gui package. 
+ * From there, the various options that appear will instantiate a window/Frame corresponding to a file in the GUI package. 
  */
 
 
